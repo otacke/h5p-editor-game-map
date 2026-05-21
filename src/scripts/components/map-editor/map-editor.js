5c50d796ca4a2b87f7a85bb8c3817cd03e752b8b
Oliver Tacke <o.tacke@posteo.de>
2026-05-21 19:21:01 +0200

    Remove debug output

--- src/scripts/components/map-editor/map-editor.js
+++ src/scripts/components/map-editor/map-editor.js
@@ -282,8 +282,6 @@ export default class MapEditor {
    *
    */
   validateMapElements() {
-    console.log('validate');
-
     this.mapElements.forEach((mapElement) => {
       mapElement.validate();
     });
